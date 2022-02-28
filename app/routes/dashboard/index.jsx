1a490d3eaf06dac2d4c2cb7f8ce223165d5bbc17
andrew fauth <andrewlfauth@gmail.com>
2022-02-27 22:44:19 -0800

    user avatar

--- app/routes/dashboard/index.jsx
+++ app/routes/dashboard/index.jsx
@@ -1,15 +1,20 @@
 import {useLoaderData, redirect, Form} from 'remix'
+import {useState, useEffect} from 'react'
 import saveProfile from '../../utils/saveProfile'
 import Users from '../../models/Users'
 import Shows from '../../models/Shows'
 import ProfilesBlock from '../../components/ProfilesBlock'
 import MyFavoritesBlock from '../../components/MyFavoritesBlock'
 import LinkBlock from '../../components/LinkBlock'
+import AlertPopup from '~/components/AlertPopup'
+import Axios from 'axios'
+import dog from '~/assets/dogIcon.svg'
 
 export async function action({request}) {
   const data = await request.formData()
   const {...values} = Object.fromEntries(data)
-  return saveProfile(values)
+
+  return saveProfile(values)  
 }
 
 export async function loader({request}) {
@@ -28,17 +33,74 @@ export async function loader({request}) {
 function Dashboard() {
   const user = useLoaderData().user
   const favorites = useLoaderData().favorites
+  const [imageId, setImageId] = useState(false)
+  const [imageDidLoad, setImageDidLoad] = useState(false)
+  const [updatingImage, setUpdatingImage] = useState(false)
+  const [invalidImage, setInvalidImage] = useState(false)
+  let imgUrl = 'https://res.cloudinary.com/dpnkrz8c8/image/upload/c_thumb,g_face,w_85,h_85,r_max/v1645945686/'
+  
+  useEffect(() => {
+    setImageId(localStorage.getItem('imageId'))
+  },[])
+
+  function uploadImage(files) {
+    const path = files[0].name.split('.')
+
+    switch (path[path.length - 1]) {
+      case 'jpg':
+      case 'png':
+      case 'svg':
+        break
+      default :
+        setInvalidImage(true)
+        break
+    }
+    setUpdatingImage(true)
+    const formData = new FormData()
+    formData.append("file", files[0])
+    formData.append("upload_preset", 'owdo4rra')
+    Axios.post(
+      "https://api.cloudinary.com/v1_1/dpnkrz8c8/image/upload", 
+      formData
+    ).then(res => {
+      setImageId(res.data.public_id + '.png')
+      localStorage.setItem('imageId', res.data.public_id + '.png')
+      setUpdatingImage(false)
+    })
+  }  
   
   return (
-    <main className='px-8 mt-28'>
-      <h1 className="mx-auto mb-8 text-2xl w-fit">Dashboard</h1>
+    <main className='relative px-8 mt-28'>
+      {invalidImage && 
+        <AlertPopup 
+          type='error'
+          title='Woops!'
+          message='Not a valid image format'
+          duration={5000}
+        />
+      }
+      <h1 className="mb-4 text-3xl text-center text-slate-900">Dashboard</h1>
+
+      <label htmlFor="avatarFile">
+      <img  
+        src={imageId ? imgUrl + imageId : dog} alt="profile picture" 
+        className={`${imageDidLoad ? '' : 'h-20'} ${updatingImage && 'animate-pulse'} mx-auto mb-3`}
+      />
+      </label>
+      <input
+        className='hidden' 
+        type='file' name="avatar" id="avatarFile"
+        onChange={e => uploadImage(e.target.files)} 
+      />
+
       <ProfilesBlock user={user} />    
       <MyFavoritesBlock user={user} favorites={favorites} />
       <LinkBlock title='Discover' to='/shows' />   
       <Form action='/logout' method='post'>
         <button
           type='submit' 
-          className="w-full py-2 mt-4 text-lg tracking-wide text-white rounded-md bg-slate-900 dark:bg-slate-700">
+          className="w-full py-2 mt-4 text-lg tracking-wide text-white rounded-md bg-slate-900 dark:bg-slate-700"
+        >
           Logout
         </button> 
       </Form>
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,32 +1,30 @@
-import {UserContext} from '../root'
-import {useContext} from 'react'
 import girlOnTablet from '../assets/kids-on-phone.jpg'
 import {Link} from 'remix'
 import {BsArrowRightCircleFill} from 'react-icons/bs'
 
-
 function Index() {
+
   return (    
     <div className="relative px-10 mt-28 ">
-    <h1 className='text-2xl text-slate-900 leading-normal transition-colors duration-700 delay-500 md:text-4xl dark:text-white'>
+    <h1 className='text-2xl leading-normal transition-colors duration-700 delay-500 text-slate-900 md:text-4xl dark:text-white'>
       Make the most of their screen time
     </h1>
     <Link to='register'>
       <button 
-        className='w-full py-2 mt-4 text-lg tracking-wide text-white rounded-md bg-slate-900 transition-colors duration-700 delay-500'
+        className='w-full py-2 mt-4 text-lg tracking-wide text-white transition-colors duration-700 delay-500 rounded-md bg-slate-900'
       >
         Get Started
       </button><br />
     </Link>
     <Link to='/shows'>
       <button 
-        className='w-full py-1 mt-4 text-lg tracking-wide text-slate-900 rounded-md border-4 border-slate-900 transition-colors duration-700 delay-500'
+        className='w-full py-1 mt-4 text-lg tracking-wide transition-colors duration-700 delay-500 border-4 rounded-md text-slate-900 border-slate-900'
       >
         Browse
       </button>
     </Link>
     <img 
-      className='w-11/12 mt-20 mx-auto rounded-lg shadow-md' 
+      className='w-11/12 mx-auto mt-20 rounded-lg shadow-md' 
       src={girlOnTablet} alt="young child using a tablet" 
       title='Photo by McKaela Taylor on Unsplash'
     />
